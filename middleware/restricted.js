1861ce9579ff78010a06bfe943b4225237c9736b
Ian <mus5002@aol.com>
2020-01-21 21:06:44 -0500

    added migrations, and modified routes

--- middleware/restricted.js
+++ middleware/restricted.js
@@ -1,10 +1,18 @@
+const jwt = require('jsonwebtoken')
+const secrets = require('../config/secrets')
+
 module.exports = () => {
-        return (req, res, next) => {
-        if (!req.session || !req.session.user) {
+    return (req, res, next) => {
+        try {
+            const token = req.headers.authorization
+            const decoded = jwt.verify(token, secrets.jwt)
+
+            req.userId = decoded.subject
+            next()
+        } catch (err) {
             return res.status(401).json({
-                message: 'Invalid Credentials'
+                message: 'Invalid credentials',
             })
         }
-        next()
     }
 }
